96e5fe8ba90579efd876e3d63d9c360096e4e4e7
asv.spb <asv.devpro@gmail.com>
2022-10-28 01:28:27 +0300

    carousel section finished

---
+++ src/js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $('.carousel__items').slick({
+    speed: 1200,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="./img/svg/left-arr.svg" alt="slide-arr-left"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="./img/svg/right-arr.svg" alt="slide-arr-right"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          }
+      }
+    ],
+  });
+});
